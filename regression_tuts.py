7aab58f08de97563b71f9a343ca13b2a8e5310aa
goktugisiklar <isiklar.goktug@metu.edu.tr>
2020-10-28 11:53:00 +0300

    Forecasting for following 30 days

--- regression_tuts.py
+++ regression_tuts.py
@@ -2,8 +2,13 @@ import pandas as pd
 import quandl
 import math
 import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+
+style.use('ggplot')
 
 df = quandl.get("WIKI/GOOGL")
 
@@ -19,15 +24,27 @@ df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
-X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
+
+X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
