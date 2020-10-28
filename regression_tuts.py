e8383f066050d34924c4ca35ca0719ab04ff1b5c
goktugisiklar <isiklar.goktug@metu.edu.tr>
2020-10-28 12:35:30 +0300

    Use Pickle for already trained sets

--- regression_tuts.py
+++ regression_tuts.py
@@ -1,9 +1,10 @@
-import pandas as pd
+import datetime
 import quandl
 import math
-import numpy as np
-import datetime
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pickle
 from matplotlib import style
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
@@ -18,13 +19,11 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-
 forecast_col = 'Adj. Close'
 df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
@@ -33,12 +32,17 @@ X_lately = X[-forecast_out:]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
-
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+## Comment out if you trained your set 
 clf = LinearRegression(n_jobs=-1)
 clf.fit(X_train, y_train)
-accuracy = clf.score(X_test, y_test)
+with open('linearregression.pickle','wb') as f:
+    pickle.dump(clf, f)
 
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
+
+accuracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
 print(forecast_set, accuracy, forecast_out)
 df['Forecast'] = np.nan
